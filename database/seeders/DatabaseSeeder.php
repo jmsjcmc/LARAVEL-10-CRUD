d40b3a3d834bc37d7fb9dec38d68e8348b8333a9
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-11-24 16:53:11 +0800

    refactor: use first name and last for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'username' => 'tester123',
         ]);
     }
 }
